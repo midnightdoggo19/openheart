b338241fe306b9c6b0523a763ee04949a3497566
Midnight Doggo <git@midnightdoggo19.com>
2025-07-28 14:45:55 +0100

    Removed ability to search by tag (1.21.3)

--- src/main/java/com/midnightdoggo19/openheart/BlockLocator.java
+++ src/main/java/com/midnightdoggo19/openheart/BlockLocator.java
@@ -2,7 +2,7 @@ package com.midnightdoggo19.openheart;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.MinecraftClient;
+//import net.minecraft.client.MinecraftClient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -15,30 +15,6 @@ import java.util.concurrent.Executors;
 
 public class BlockLocator {
     private static final Executor EXECUTOR = Executors.newSingleThreadExecutor();
-
-    // search by tag
-    public static CompletableFuture<BlockPos> findNearestBlockByTagAsync(World world, BlockPos origin, TagKey<Block> blockTag, int maxRadius) {
-        return CompletableFuture.supplyAsync(() -> {
-            for (int radius = 1; radius <= maxRadius; radius++) {
-                for (int x = -radius; x <= radius; x++) {
-                    for (int y = -radius; y <= radius; y++) {
-                        for (int z = -radius; z <= radius; z++) {
-                            if (Math.abs(x) != radius && Math.abs(y) != radius && Math.abs(z) != radius) continue;
-
-                            BlockPos currentPos = origin.add(x, y, z);
-                            if (!world.isChunkLoaded(currentPos)) continue;
-
-                            BlockState state = world.getBlockState(currentPos);
-                            if (state.isIn(blockTag)) {
-                                return currentPos;
-                            }
-                        }
-                    }
-                }
-            }
-            return null;
-        }, EXECUTOR);
-    }
 
     // search by type (minecraft:air)
     public static CompletableFuture<BlockPos> findNearestBlockByTypeAsync(World world, BlockPos origin, Block block, int maxRadius) {
